03e2ae8ffd9037a68f196af01c2ce1e913f8828b
Will Simm <will@wasdesign.net>
2015-03-19 11:49:50 +0000

    Create sleepshape.js

---
+++ sleepshape.js
@@ -0,0 +1,52 @@
+var voxelSize = 0.08*MM;
+
+
+function createShape(size,score,x,y,z){
+  // the cube for the dice
+  //var size = args[0];
+  var box = new Box(x,y,z,size,size,size);
+  
+  // round the corners and edges by intersecting cube with sphere
+  // sphere with radius 32% of cube length is sufficient
+  var fudge = (score / 10 *0.35) +0.1;
+  var radius = size -fudge*size;
+  //var radius = size - (score/5)*size;
+  var sphere = new Sphere(x,y,z,radius);
+  var intersect = new Intersection();
+  intersect.add(box);
+  intersect.add(sphere);
+  return intersect;
+
+
+
+}
+
+
+
+
+function main(args) {
+  // the cube for the dice
+  var size = args[0];
+  var union=Union();
+  var x=0;
+  var y=0;
+  var z=0;
+
+  
+  //step through args makinh shapes and adding to union
+  for (index = 1; index < args.length; ++index) {
+    union.add(createShape(size,args[index],x,y,z));
+    x=x+size/2;
+    y=y+size/2;
+    z=z+size/2;
+
+  }
+        
+  
+  var halfgrid = size *2;
+  var grid = createGrid(-halfgrid,halfgrid,-halfgrid,halfgrid,-halfgrid,halfgrid,voxelSize);
+  var maker = new GridMaker();
+  maker.setSource(union);
+  maker.makeGrid(grid);
+  return grid;
+}
